c8f9402c1fd57e54bc1853ebbff67efaca1c740a
Pranav Negandhi <pranav@vertika.tv>
2023-01-16 17:13:26 +0530

    Changed the implementation of the active states to switch to the AppReadyState if the session is cancelled.

--- src/Core/LongBreakState.cs
+++ src/Core/LongBreakState.cs
@@ -22,7 +22,7 @@
 
                 if (cancellationToken.IsCancellationRequested)
                 {
-                    return;
+                    break;
                 }
             }
 
--- src/Core/WorkSessionState.cs
+++ src/Core/WorkSessionState.cs
@@ -13,20 +13,23 @@
         public override async Task EnterAsync(CancellationToken cancellationToken)
         {
             _log.Verbose("Entering {state}", nameof(WorkSessionState));
+            Notification? notification = null;
 
             while (_timeElapsed < StateDuration)
             {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    notification = new Notification(new AppReadyState(0, _settingsFactory, _queue));
+                    break;
+                }
+
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
                 _timeElapsed = _timeElapsed.Add(TimeSpan.FromSeconds(1));
                 OnProgress(_timeElapsed, StateDuration);
-
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    return;
-                }
             }
 
-            _queue.Enqueue(new Notification(new WorkCompletedState(RoundCounter, _settingsFactory, _queue)));
+            notification ??= new Notification(new WorkCompletedState(RoundCounter, _settingsFactory, _queue));
+            _queue.Enqueue(notification);
         }
 
         public override EngineState State => EngineState.WorkSession;
