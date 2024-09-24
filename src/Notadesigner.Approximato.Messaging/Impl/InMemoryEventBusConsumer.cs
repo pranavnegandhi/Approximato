b948c039de4c117f77196e2ae24ccab712b20996
Pranav Negandhi <pranav.negandhi@sarvaha.com>
2024-09-24 18:41:27 +0530

    #43 -  Utilise a one-to-many message dispatch architecture to distribute state machine events to components within the application
    Removed the unnecessary async modifier from the StartAsync method.

--- src/Notadesigner.Approximato.Messaging/Impl/InMemoryEventBusConsumer.cs
+++ src/Notadesigner.Approximato.Messaging/Impl/InMemoryEventBusConsumer.cs
@@ -22,7 +22,7 @@ namespace Notadesigner.Approximato.Messaging.Impl
             return ValueTask.CompletedTask;
         }
 
-        public async ValueTask StartAsync(CancellationToken cancellationToken = default)
+        public ValueTask StartAsync(CancellationToken cancellationToken = default)
         {
             EnsureStoppingTokenCreated();
 
@@ -33,7 +33,7 @@ namespace Notadesigner.Approximato.Messaging.Impl
             {
                 _logger.Debug("No handlers defined for {@EventType}", typeof(T).Name);
 
-                return;
+                return ValueTask.CompletedTask;
             }
 
             _ = Task.Run(async () =>
@@ -41,51 +41,53 @@ namespace Notadesigner.Approximato.Messaging.Impl
                 await StartProcessingAsync(handlers, contextAccessor).ConfigureAwait(false);
             }, _stoppingCts!.Token)
                 .ConfigureAwait(false);
-    }
 
-    public async ValueTask StopAsync(CancellationToken cancellationToken = default)
-    {
-        await DisposeAsync().ConfigureAwait(false);
-    }
-
-    private void EnsureStoppingTokenCreated()
-    {
-        if (_stoppingCts is not null && !_stoppingCts.IsCancellationRequested)
-        {
-            _stoppingCts.Cancel();
+            return ValueTask.CompletedTask;
         }
 
-        _stoppingCts = new();
-    }
-
-    private async Task StartProcessingAsync(IEnumerable<IEventHandler<T>> handlers, IEventContextAccessor<T> contextAccessor)
-    {
-        var iterator = _bus.ReadAllAsync(_stoppingCts!.Token)
-            .WithCancellation(_stoppingCts.Token)
-            .ConfigureAwait(false);
-
-        await foreach (var task in iterator)
+        public async ValueTask StopAsync(CancellationToken cancellationToken = default)
         {
-            if (_stoppingCts.IsCancellationRequested)
+            await DisposeAsync().ConfigureAwait(false);
+        }
+
+        private void EnsureStoppingTokenCreated()
+        {
+            if (_stoppingCts is not null && !_stoppingCts.IsCancellationRequested)
             {
-                break;
+                _stoppingCts.Cancel();
             }
 
-            await Parallel.ForEachAsync(handlers,
-                _stoppingCts.Token,
-                async (handler, scopedToken) => await ExecuteHandlerAsync(handler, task, contextAccessor, scopedToken).ConfigureAwait(false))
+            _stoppingCts = new();
+        }
+
+        private async Task StartProcessingAsync(IEnumerable<IEventHandler<T>> handlers, IEventContextAccessor<T> contextAccessor)
+        {
+            var iterator = _bus.ReadAllAsync(_stoppingCts!.Token)
+                .WithCancellation(_stoppingCts.Token)
                 .ConfigureAwait(false);
+
+            await foreach (var task in iterator)
+            {
+                if (_stoppingCts.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                await Parallel.ForEachAsync(handlers,
+                    _stoppingCts.Token,
+                    async (handler, scopedToken) => await ExecuteHandlerAsync(handler, task, contextAccessor, scopedToken).ConfigureAwait(false))
+                    .ConfigureAwait(false);
+            }
+        }
+
+        private ValueTask ExecuteHandlerAsync(IEventHandler<T> handler, Event<T> task, IEventContextAccessor<T> contextAccessor, CancellationToken cancellationToken)
+        {
+            contextAccessor.Set(task); // set metadata and begin scope
+
+            _ = Task.Run(async () => await handler.HandleAsync(task.Data, cancellationToken), cancellationToken)
+                .ConfigureAwait(false);
+
+            return ValueTask.CompletedTask;
         }
     }
-
-    private ValueTask ExecuteHandlerAsync(IEventHandler<T> handler, Event<T> task, IEventContextAccessor<T> contextAccessor, CancellationToken cancellationToken)
-    {
-        contextAccessor.Set(task); // set metadata and begin scope
-
-        _ = Task.Run(async () => await handler.HandleAsync(task.Data, cancellationToken), cancellationToken)
-            .ConfigureAwait(false);
-
-        return ValueTask.CompletedTask;
-    }
-}
 }
